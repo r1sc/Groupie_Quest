c18d8ec4c3ba62bb6ca83a92de6997e1edd86c59
r1sc <629669+r1sc@users.noreply.github.com>
2023-12-31 22:15:23 +0100

    Cleanup

--- Groupie_Quest.lua
+++ Groupie_Quest.lua
@@ -1,5 +1,5 @@
 Groupie_Debug = false
-Groupie_XPs = {}
+Groupie_Quests = {}
 
 local debug_print = function(...)
     if Groupie_Debug then
@@ -7,44 +7,9 @@ local debug_print = function(...)
     end
 end
 
-function SendXP()
-    local xp = UnitXP("player")
-    local xpMax = UnitXPMax("player")
-    Groupie_SendAddonMessage("Groupie_XP", xp.."|"..xpMax, "PARTY")
-end
-
-function Groupie_SendAddonMessage(prefix, message, channel)
+local send = function(prefix, message, channel)
     debug_print("SendAddonMessage("..prefix..","..message..","..channel..")")
     C_ChatInfo.SendAddonMessage(prefix, message, channel)    
-end
-
-function RefreshFrame(expMarker, portraitTextureName, name, xpTable)
-	SetPortraitTexture(expMarker.portrait, portraitTextureName)
-
-	local expPercent = (xpTable.xp / xpTable.xpMax)
-	expMarker:SetPoint("CENTER", MainMenuExpBar, "LEFT", expPercent * MainMenuExpBar:GetWidth(), 0)
-	
-	expMarker.playerName = name.." "..floor(expPercent * 100).."%"
-	
-	expMarker:Show()
-end
-
-function RefreshPartyXPBars()
-    local numMembers = GetNumGroupMembers()
-    for i=1,numMembers-1 do
-        local name_i =  UnitName("party"..i)
-        local expMarker = _G["PartyMember"..i.."ExpMarker"]
-        local partyMemberXp = Groupie_XPs[name_i]
-		
-        if partyMemberXp ~= nil then            
-            debug_print("Updating xp bar for "..name_i)
-			RefreshFrame(expMarker, "party"..i, name_i, partyMemberXp)
-            
-        else
-            debug_print("No xp data for "..name_i)
-            expMarker:Hide()
-        end
-    end
 end
 
 function GetLeaderBoardDetails (boardIndex,questIndex)
@@ -56,13 +21,12 @@ function GetLeaderBoardDetails (boardIndex,questIndex)
 
 -- Event handling
 
-Groupie_Quests = {}
 
 local events = {
     ADDON_LOADED = function(addonName)
-        if addonName == "Groupie" then            
-            local result = C_ChatInfo.RegisterAddonMessagePrefix("Groupie_XP")
-            print("Groupie: Hello world! Registered addon message: "..(result and "yes" or "no"))
+        if addonName == "Groupie_Quest" then            
+            local result = C_ChatInfo.RegisterAddonMessagePrefix("Groupie_Quest")
+            print("Groupie Quest: Loaded")
             
             numEntries, _ = GetNumQuestLogEntries()
             for questLogIndex = 1,numEntries do
@@ -102,11 +66,11 @@ local events = {
 
 --- Set up frame etc.
 
-CreateFrame("Frame", "Groupie", UIParent)
+CreateFrame("Frame", "Groupie_Quest", UIParent)
 for e,f in pairs(events) do
-    Groupie:RegisterEvent(e)
+    Groupie_Quest:RegisterEvent(e)
 end
-Groupie:SetScript("OnEvent", function(self, event, ...)
+Groupie_Quest:SetScript("OnEvent", function(self, event, ...)
     for e,f in pairs(events) do
         if e == event then
             f(...)
@@ -115,24 +79,25 @@ Groupie:SetScript("OnEvent", function(self, event, ...)
     end
 end)
 
--- Set up party experience frames
-for i=1,4 do    
-    local f = CreateFrame("Frame", "PartyMember"..i.."ExpMarker", MainMenuExpBar)
+-- Set up quest tracker info frames
+for i=2,10 do    -- index 1 is always a header so skip that one
+    local questWatchLine = _G["QuestWatchLine"..i]
+    local f = CreateFrame("Frame", "GroupieQuestInfo"..i, QuestWatchFrame)
     f:SetWidth(20)
     f:SetHeight(20)
     f:SetFrameStrata("HIGH")
 
-	f:SetScript("OnEnter", function(self) 
-		ShowUIPanel(GameTooltip)
-		GameTooltip:SetOwner(self, "ANCHOR_CURSOR")
-		GameTooltip:SetText(self.playerName)
-		GameTooltip:Show()
-	end)
-	f:SetScript("OnLeave", function(self) 
-		GameTooltip:Hide()
-	end)
+	-- f:SetScript("OnEnter", function(self) 
+	-- 	ShowUIPanel(GameTooltip)
+	-- 	GameTooltip:SetOwner(self, "ANCHOR_CURSOR")
+	-- 	GameTooltip:SetText(self.questInfo)
+	-- 	GameTooltip:Show()
+	-- end)
+	-- f:SetScript("OnLeave", function(self) 
+	-- 	GameTooltip:Hide()
+	-- end)
 
-    f:SetPoint("CENTER", MainMenuExpBar, "LEFT", 0, 0)
+    f:SetPoint("CENTER", questWatchLine, "LEFT", 0, 0)
 
     f.bg = f:CreateTexture(nil, "MEDIUM")
     f.bg:SetTexture("Interface/Common/BlueMenuRing")
@@ -140,7 +105,5 @@ for i=1,4 do
     f.bg:SetWidth(38)
     f.bg:SetHeight(38)
 
-    f.portrait = f:CreateTexture(nil, "BACKGROUND")
-    f.portrait:SetAllPoints(true)
-    f:Hide()
+    f:Show()
 end
